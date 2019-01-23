28bb858fec2fef6ff8606a559c5cd3ef4442468c
wangsifang <wangsifang@huaxsoft.com>
2019-01-23 10:03:50 +0800

    cli命令

--- JavaScript-Everywhere/index.js
+++ JavaScript-Everywhere/index.js
@@ -12,10 +12,12 @@ fs.readdir(process.cwd(), (err, files) => {
 
     console.log(' Select which file or directory you want to see \n')
 
+    let stats = []
     function file(i) {
         let filename = files[i]
     
         fs.stat(__dirname + '/' + filename, (err, stat) => {
+            stats[i] = stat
             if (stat.isDirectory()) {
                 console.log(`${i}  ${filename}`.yellow)
             }else{
@@ -41,19 +43,31 @@ fs.readdir(process.cwd(), (err, files) => {
     }  
 
     function option(data) {
-        console.log(data)
         let filename = files[Number(data)]
         if (!filename) {
             stdout.write('Enter your choice again: '.cyan)
         } else {
             stdin.pause()
 
-            fs.readFile(`${__dirname}/${filename}`, 'utf8', (err, data) => {
-                if (err) return console.log(err) 
+            if (stats[Number(data)].isDirectory()) {
+                fs.readdir(`${__dirname}/${filename}`, (err, files) => {
+                    if (err) return console.log(err) 
+                    console.log('')
+                    
+                    console.log(`Total( ${files.length} ) files`)
 
-                console.log('')
-                console.log(data)
-            })
+                    files.map(item => {
+                        console.log('   - ' + item)
+                    })
+                    console.log('')
+                })
+            } else {
+                fs.readFile(`${__dirname}/${filename}`, 'utf8', (err, data) => {
+                    if (err) return console.log(err) 
+                    console.log('')
+                    console.log(data)
+                })
+            }
         }
     }
 
